e0fd376e81e506548ec3b3c287fce3fb628a70de
vkasyan76 <vkasyan@gmx.de>
2022-06-11 21:16:25 +0200

    Update#2

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -21,4 +21,4 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 const analytics = getAnalytics(app)
-export const db = getFirestore()
+export const db = getFirestore(app)
